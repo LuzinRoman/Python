3d3154c1b910cf3e09b27c964d767dc5b2595319
Roman <rluzin@yandex.ru>
2023-08-26 19:52:52 +0300

    Решили задачу №12 с Петей и Катей

--- HomeWork.py
+++ HomeWork.py
@@ -4,16 +4,38 @@
 # той же стороной. Выведите минимальное количество монет, 
 # которые нужно перевернуть
 
-n = int(input("Введите количество монеток: "))
-reshka_count = 0
-gerb_count = 0
+# n = int(input("Введите количество монеток: "))
+# reshka_count = 0
+# gerb_count = 0
 
-for i in range(n):
-    side = int(input(f"Введите сторону монетки (0 - решка, 1 - герб): "))
-    if side == 0:
-        reshka_count += 1
-    else:
-        gerb_count += 1
+# for i in range(n):
+#     side = int(input(f"Введите сторону монетки (0 - решка, 1 - герб): "))
+#     if side == 0:
+#         reshka_count += 1
+#     else:
+#         gerb_count += 1
 
-min_coins = min(reshka_count, gerb_count)
-print(f"Минимальное количество монет, которые нужно перевернуть: {min_coins}")
+# min_coins = min(reshka_count, gerb_count)
+# print(f"Минимальное количество монет, которые нужно перевернуть: {min_coins}")
+
+# Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. 
+# Петя помогает Кате по математике. Он задумывает два натуральных числа
+# X и Y (X,Y≤1000), а Катя должна их отгадать. 
+# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и
+# их произведение P.                           
+# Помогите Кате отгадать задуманные Петей числа.
+
+
+# s = int(input('Задай сумму двух чисел: '))
+# p = int(input('Задай произведение чисел: '))
+ 
+# found_numbers = False
+ 
+# for x in range(1, s):
+#     for y in range(1, s):
+#         if s == x + y and p == x * y:
+#             print(f'Первое число: {x}, второе число: {y}')
+#             found_numbers = True
+ 
+# if not found_numbers:
+#     print('Числа X и Y не удалось найти.')
