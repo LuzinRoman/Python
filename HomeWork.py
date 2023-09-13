473c5c695e6ac8e0856687910cf413b3edd5fed9
Roman <rluzin@yandex.ru>
2023-09-13 13:23:04 +0300

    Решили д/з №36

--- HomeWork.py
+++ HomeWork.py
@@ -163,20 +163,48 @@
 
 # **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
 #     **Вывод:** Парам пам-пам  
-poem_input = input("Введите стихотворение: ")
+# poem_input = input("Введите стихотворение: ")
 
-def rhythm(poem):
-    poem = poem.split()
-    vowel_counts = []
-    for word in poem:
-        count = 0
-        for letter in word:
-            if letter in 'аеёиоуыэюя':
-                count += 1
-        vowel_counts.append(count)
-    return len(vowel_counts) == vowel_counts.count(vowel_counts[0])
+# def rhythm(poem):
+#     poem = poem.split()
+#     vowel_counts = []
+#     for word in poem:
+#         count = 0
+#         for letter in word:
+#             if letter in 'аеёиоуыэюя':
+#                 count += 1
+#         vowel_counts.append(count)
+#     return len(vowel_counts) == vowel_counts.count(vowel_counts[0])
 
-if rhythm(poem_input):
-    print('Парам пам-пам')
-else:
-    print('Пам парам')
+# if rhythm(poem_input):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+# Задача 36: Напишите функцию print_operation_table
+# (operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента 
+# функцию, вычисляющую элемент по номеру строки и столбца. 
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, 
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы 
+# (подумайте, почему не с нуля). Примечание: бинарной операцией называется любая
+# операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+# *Пример:*
+
+# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+# **Вывод:**
+# 1 2 3 4 5 6
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows + 1):
+        row = []
+        for j in range(1, num_columns + 1):
+            row.append(operation(i, j))
+        print(*row)
+
+print_operation_table(lambda x, y: x * y)
